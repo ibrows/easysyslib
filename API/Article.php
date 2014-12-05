259dc2ffdfb879e9c089f93f82f32d9d6a4d6bae
Dominik Zogg <dominik.zogg@gmail.com>
2014-12-05 11:59:54 +0100

    add article api

---
+++ API/Article.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ibrows\EasySysLibrary\API;
+
+class Article extends AbstractAPI
+{
+    /**
+     * @return string
+     */
+    protected function getType()
+    {
+        return 'article';
+    }
+}
