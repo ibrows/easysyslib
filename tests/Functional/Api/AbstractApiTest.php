e1225e93026cad69d75a6a6be16f041e8db33d79
Mike Meier <mike.meier@ibrows.ch>
2014-12-16 18:57:47 +0100

    enable file logger for functional testing

--- tests/Functional/Api/AbstractApiTest.php
+++ tests/Functional/Api/AbstractApiTest.php
@@ -13,9 +13,15 @@ namespace Ibrows\EasySysLibrary\Tests\Functional\Api;
 
 use Ibrows\EasySysLibrary\Connection\ConnectionInterface;
 use Ibrows\EasySysLibrary\Tests\Functional\ConnectionTest;
+use Ibrows\EasySysLibrary\Tests\Logger\FileLogger;
 
 abstract class AbstractApiTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var FileLogger
+     */
+    protected static $logger;
+
     /**
      * @return ConnectionInterface|null
      */
@@ -26,6 +32,12 @@ abstract class AbstractApiTest extends \PHPUnit_Framework_TestCase
                 'The credentials.ini File is not available or wrong'
             );
         }
+        $connection->setLogger($this->getLogger());
         return $connection;
+    }
+
+    protected function getLogger()
+    {
+        return self::$logger = self::$logger ?: new FileLogger();
     }
 } 
---
+++ tests/Logger/FileLogger.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Project: coffeeconnection
+ * 
+ * User: mikemeier
+ * Date: 16.12.14
+ * Time: 18:46
+ */
+
+namespace Ibrows\EasySysLibrary\Tests\Logger;
+
+use Psr\Log\LoggerInterface;
+
+class FileLogger implements LoggerInterface
+{
+    /**
+     * @var string
+     */
+    protected $file;
+
+    public function __construct()
+    {
+        $this->file = __DIR__.'/output/'. date('Y-m-d_H_i_s').'.log';
+    }
+
+    /**
+     * @param string $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->log('emergency', $message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->log('alert', $message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->log('critical', $message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function error($message, array $context = array())
+    {
+        $this->log('error', $message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->log('warning', $message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->log('notice', $message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        $this->log('info', $message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->log('debug', $message, $context);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $date = date('Y-m-d H:i:s');
+        file_put_contents($this->file, "$date: [$level] $message - ". json_encode($context) ."\n", FILE_APPEND);
+    }
+} 
