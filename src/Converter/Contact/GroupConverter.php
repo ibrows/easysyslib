a094a5637f12c6e07b5345822350b7f70f3104cc
Stefan Vetsch <stefan.vetsch@ibrows.ch>
2015-07-10 10:10:53 +0200

    Add API support for receiving sectors (Branchen) and groups.

---
+++ src/Converter/Contact/GroupConverter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stefanvetsch
+ * Date: 09.07.15
+ * Time: 16:03
+ */
+
+namespace Ibrows\EasySysLibrary\Converter\Contact;
+
+
+use Ibrows\EasySysLibrary\Converter\AbstractSimpleConverter;
+
+class GroupConverter extends AbstractSimpleConverter
+{
+    protected $modelClass = 'Ibrows\EasySysLibrary\Model\Contact\Group';
+}
